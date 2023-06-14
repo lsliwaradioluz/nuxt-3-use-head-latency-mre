768ee8f7eec1efabd2bc653cad708310b6d20679
Łukasz Śliwa <lsliwaradioluz@gmail.com>
2023-06-14 22:38:46 +0200

    feat: reproduction

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>
+    <nuxt-link to="/">Home</nuxt-link>
+  </p>
+  <p>
+    <nuxt-link to="/test">Test</nuxt-link>
+  </p>
+
+  <NuxtPage />
+</template>
---
+++ pages/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 :class="`heading-${id}`">Test</h1>
+    <p>Test page</p>
+  </div>
+</template>
+
+<script setup>
+const id = '12345';
+
+useHead({
+  style: [{
+    id,
+    textContent: `
+      .heading-${id} {
+        color: red;
+      }
+    `
+  }]
+})
+</script>
